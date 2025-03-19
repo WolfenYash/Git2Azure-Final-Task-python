7eeb9a6ca008c818c49bd77a1cecd67202a87f58
GitStudentHub <githubamityofficial@gmail.com>
2025-03-20 00:23:18 +0530

    Update fixme.c

--- fixme.c
+++ fixme.c
@@ -2,7 +2,7 @@
 
 int fibonacci(int n) {
     if (n <= 0) return 0;
-    if (n == 1) return 1; //Fix me
+    if (n == 1) return 1; 
     
     int a = 0, b = 1, temp;
     for (int i = 2; i <= n; i++) {
